2b9730320e23e8d6481f3170e3a88070f67d02a6
Mathiyazhagan <programmermathi@gmail.com>
2025-12-24 21:20:18 +0530

    hi

---
+++ odd_even.py
@@ -0,0 +1,7 @@
+print("Enter the number to find the value is odd or even:")
+num =int(input())
+if num % 2 == 0:
+    print(f"{num} is an Even number")
+else :
+    print(f"{num} is an Odd number")
+print("hi")
